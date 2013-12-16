d44f845097bec9ba2f98b9a6c5c13ef9538579fd
Esa Varemo <esa@kuivanto.fi>
2013-12-17 00:44:09 +0200

    Add stuff accidentally deleted in the last commit

--- scripts/modify.sh
+++ scripts/modify.sh
@@ -3,4 +3,4 @@
 BASE=/home/pi/site/
 
 cp $BASE/resources/index.html $BASE/sync/alternative/
-cat $BASE/resources/custom.css >> $BASE/sync/alternative/@Style/`ls $BASE/sync/alternative/@Style/`
+#cat $BASE/resources/custom.css >> $BASE/sync/alternative/@Style/`ls $BASE/sync/alternative/@Style/`
