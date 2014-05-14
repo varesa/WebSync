6dc2f9b69e64b661c9bb7d14b799a1d055319a7c
Esa Varemo <esa@kuivanto.fi>
2014-05-14 17:58:17 +0300

    Check if another sync is in progress before starting

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Check for duplicate process
+
+pids="$(pidof -x update.sh)"
+count="$(echo \"$pids\" | wc -w)"
+
+case "$count" in
+1)  echo "starting download" # I am the only on running
+    ;;
+*)  echo "already running, aborting"
+    exit
+    ;;
+esac
+
 BASE=/home/pi/site/scripts/
 cd $BASE
 
