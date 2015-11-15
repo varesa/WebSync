1dd05cbe66f3ec3ccf5682765227b5d73bc59ecc
Esa Varemo <esa@kuivanto.fi>
2015-11-15 20:00:18 +0200

    Find directory of the script

--- scripts/update.sh
+++ scripts/update.sh
@@ -13,8 +13,14 @@ case "$count" in
     ;;
 esac
 
-BASE=/home/pi/site/scripts/
-cd $BASE
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd $DIR
 
 ./check_connection.py 1>/dev/null
 ./get_urls.py 1>/dev/null
