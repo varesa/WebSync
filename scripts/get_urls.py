5cec86c2419fc7ea13acf1e5b82c87c36554e74b
Esa Varemo <esa@kuivanto.fi>
2019-11-23 14:34:07 +0200

    Allow bypassing of certificate verification (if network uses SSL Decryption)

--- scripts/get_urls.py
+++ scripts/get_urls.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from urllib.request import urlopen
+import os
+import ssl
 
 def get_id():
     file = open("/boot/NAYTON_NIMI.txt", "r")
@@ -11,7 +13,13 @@ def get_id():
 
 def get_pages(id):
     content = []
-    for line in urlopen('https://nastori2.esav.fi/get_pages/?device=' + id):
+    if not os.path.exists('/boot/HTTPS_INSECURE'):
+        urls = urlopen('https://nastori2.esav.fi/get_pages/?device=' + id)
+    else:
+        urls = urlopen('https://nastori2.esav.fi/get_pages/?device=' + id,
+                context=ssl._create_unverified_context())
+
+    for line in urls
         content.append(line.decode('UTF-8'))
     return content
 
